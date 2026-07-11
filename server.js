94c762362da1d390cd2be167179bdd986149a811
deysifortuna24 <132461977+deysifortuna24@users.noreply.github.com>
2026-07-11 10:48:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+// Increase JSON/urlencoded body size limits to allow image/base64 payloads
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Test endpoint
 app.get("/", (req, res) => {
